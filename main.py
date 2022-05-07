129c126415df3edb51ebc200aaf6c65d5fc84e72
Kyle Mohr <kylefmohr@gmail.com>
2022-05-06 19:16:03 -0500

    tons of improvements to code readability, scaling of output graph, colors, etc

--- main.py
+++ main.py
@@ -1,27 +1,16 @@
 import json
 from collections import OrderedDict
-from collections import ChainMap
-
-import numpy as np
 import matplotlib.pyplot as plt
-from matplotlib import rcParams
+from flask import Flask, render_template, request
+from werkzeug.utils import secure_filename
 
 with open('savedata.dat', 'r') as f:
     unparsed_data = f.read()
 
-
-def remove_null(stats):
-    parsedStats = {}
-    for key, value in stats.items():
-        if value is None or value == 0 or value == '':
-            pass
-        else:
-            parsedStats[key] = value
-    return parsedStats
-
-
 json_data = json.loads(unparsed_data)
 song_attempts_data = json_data['largeStringValuesContainer']['values']
+
+difficulty_index = {'easy': 0, 'medium': 1, 'hard': 2, 'expert': 3, 'XD': 4}  # save file records song attempts/completions as a 5 item array
 
 custom_song_attempts = {}
 regular_song_attempts = {}
@@ -31,55 +20,46 @@ for current_song in song_attempts_data:
     else:
         if current_song['key'].startswith('CUSTOM'):
             custom_song_attempts[current_song['key']] = current_song['val']
+        elif 'Random' in current_song['key'] or 'Calibration' in current_song['key']:   # doesn't accurately show how many times you've attempted a random song, calibration tool value seems incorrect as well
+            pass
         else:
             regular_song_attempts[current_song['key']] = current_song['val']
 
 
-expert_song_attempts = OrderedDict(sorted(regular_song_attempts.items(), key=lambda t: t[1]))
+song_attempts = OrderedDict(sorted(regular_song_attempts.items(), key=lambda t: t[1]))
 
 width = 1       # the width of the bars: can also be len(x) sequence
 
 fig, ax = plt.subplots()
 
-# ax.bar(labels, men_means, width, yerr=men_std, label='Men')
-# ax.bar(labels, women_means, width, yerr=women_std, bottom=men_means,
-#        label='Women')
-# for current_song in custom_song_attempts:
-#     print(f'{current_song} - {custom_song_attempts[current_song]}')
-#             custom_song_attempts['times_attempted'] = current_song['timesAttemptedDifficulty']
-# parsed_expert = {}
-# for current_song in expert_song_attempts["timesAttemptedDifficulty"]:
-#     print(current_song)
-expert = {}
-expert_song_attempts = json.loads(json.dumps(expert_song_attempts))
-#expert_song_attempts = expert_song_attempts.values()
-expert_song_attempts = dict(expert_song_attempts.items())
-expert = {k: v for k, v in expert_song_attempts.items() if v is not None and v != 0 and v != ''}
-#expert = expert.values()
-expert = dict(expert.items())
 
-stats = expert.values()
+song_attempts = json.loads(json.dumps(song_attempts))
+song_attempts = dict(song_attempts.items())
+song_attempts = {k: v for k, v in song_attempts.items() if v is not None and v != 0 and v != ''}
+song_attempts = dict(song_attempts.items())
+
+stats = song_attempts.values()
 new_stats = {}
+my_dpi = 108
+plt.figure(figsize=(14, 11), dpi=my_dpi)
 plt.ion()
 
 for i, current_song in enumerate(stats):
     current_song = json.loads(current_song)
-    print(current_song["statsUniqueString"][:-6])
-    #add the current song to the new_stats dict
-    new_stats[current_song["statsUniqueString"][:-6]] = current_song
-    new_stats[current_song["statsUniqueString"][:-6]]["timesAttemptedDifficulty"] = current_song["timesAttemptedDifficulty"]
-    print(current_song["timesAttemptedDifficulty"])
-    print(current_song["timesCompletedDifficulty"])
-    new_stats[current_song["statsUniqueString"][:-6]]["timesCompletedDifficulty"] = current_song["timesCompletedDifficulty"]
-    plt.xticks(rotation=90)
+    current_song_name = current_song["statsUniqueString"][:-6]
+    times_attempted = current_song["timesAttemptedDifficulty"]
+    times_completed = current_song["timesCompletedDifficulty"]
+    print(current_song_name)
+    print(times_attempted)
+    print(times_completed)
+    plt.xticks(rotation=45, ha='right', fontsize=9)
 
-    plt.bar(current_song["statsUniqueString"][:-6], current_song["timesAttemptedDifficulty"], width, label="Times Attempted")
-    plt.bar(current_song["statsUniqueString"][:-6], current_song["timesCompletedDifficulty"], width, label="Times Completed")
+    plt.bar(current_song_name, times_attempted, width, label="Times Attempted", color=(0xf7/255, 0x67/255, 0xff/255, 1), edgecolor='black')
+    plt.bar(current_song_name, times_completed, width, label="Times Completed", color=(0x57/255, 0xbe/255, 0xfc/255, 1), edgecolor='black')
 
-
-plt.subplots_adjust(bottom=0.35)
+ax = plt.gca()
 plt.show()
 
-# ax.bar(expert_song_attempts['statsUniqueString'], expert_song_attempts['timesAttemptedDifficulty'][4], width, label='Times Attempted')
-# ax.bar(expert_song_attempts['statsUniqueString'], expert_song_attempts['timesCompletedDifficulty'][4], width, bottom=expert_song_attempts['timesAttemptedDifficulty'][4], label='Times Completed')
+# ax.bar(song_attempts['statsUniqueString'], song_attempts['timesAttemptedDifficulty'][4], width, label='Times Attempted')
+# ax.bar(song_attempts['statsUniqueString'], song_attempts['timesCompletedDifficulty'][4], width, bottom=song_attempts['timesAttemptedDifficulty'][4], label='Times Completed')
 # plt.show()
