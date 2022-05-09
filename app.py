e1c7f3d45f77d2ce408fff69b5e3629ff9807d61
Kyle Mohr <6644803+kylefmohr@users.noreply.github.com>
2022-05-09 02:22:52 -0500

    fixed some incorrect assumptions made after testing other players' saves

--- app.py
+++ app.py
@@ -10,8 +10,6 @@ from flask import Flask, render_template, request, redirect
 from werkzeug.utils import secure_filename
 
 app = Flask(__name__)
-# app.config['UPLOAD_FOLDER'] = 'static/'
-# app.config['MAX_CONTENT_LENGTH'] = 1024 * 1000
 
 
 @app.route('/')
@@ -42,7 +40,7 @@ def work_with_file():
             else:
                 if current_song['key'].startswith('CUSTOM'):
                     custom_song_attempts[current_song['key']] = current_song['val']
-                elif 'Random' in current_song['key'] or 'Calibration' in current_song['key']:   # doesn't accurately show how many times you've attempted a random song, calibration tool value seems incorrect as well
+                elif 'Random' in current_song['key'] or 'Calibration' in current_song['key'] or 'Create Custom' in current_song['key'] or 'CreateCustomTrack' in current_song['key']:   # doesn't accurately show how many times you've attempted a random song, calibration tool value seems incorrect as well
                     pass
                 else:
                     regular_song_attempts[current_song['key']] = current_song['val']
@@ -51,7 +49,9 @@ def work_with_file():
         plt.ioff()
         width = 1
         my_dpi = 108
-        fig = plt.figure(figsize=(14, 11), dpi=my_dpi)
+
+        fig = plt.figure(figsize=(18, 14), dpi=my_dpi)
+
 
         song_attempts = json.loads(json.dumps(song_attempts))
         song_attempts = dict(song_attempts.items())
@@ -59,11 +59,10 @@ def work_with_file():
         song_attempts = dict(song_attempts.items())
 
         stats = song_attempts.values()
-        new_stats = {}
 
         for i, current_song in enumerate(stats):
             current_song = json.loads(current_song)
-            try:
+            try:  # needed to try/except this after testing this on other users saved data. It wasn't necessary on mine, but multiple other users ran into errors without it
                 current_song_name = current_song["statsUniqueString"][:-6]  # statsUniqueString is usually, for example, "I See Lite_Stats"
                 times_attempted = current_song["timesAttemptedDifficulty"][difficulty_index['XD']]
                 times_completed = current_song["timesCompletedDifficulty"][difficulty_index['XD']]
@@ -75,18 +74,13 @@ def work_with_file():
             print(times_completed)
             plt.xticks(rotation=45, ha='right', fontsize=9)
 
-            plt.bar(current_song_name, times_attempted, width, label="Times Attempted", color=(0xf7/255, 0x67/255, 0xff/255, 1), edgecolor='black')
-
+            plt.bar(current_song_name, times_attempted, width, label="Times Attempted", color=(0xf7/0xff, 0x67/0xff, 0xff/0xff, 1), edgecolor='black')
+            # The colors are formatted like that because I had a hexadecimal color value (0xf767ff), but matplotlib wants an RGB decimal value, where 1, 0, 1, would be 0xFF00FF. The last number is the transparency. edgecolor gives the bars an outline
             plt.bar(current_song_name, times_completed, width, label="Times Completed", color=(0x57/255, 0xbe/255, 0xfc/255, 1), edgecolor='black')
-
-
+        plt.gcf().subplots_adjust(bottom=0.14)
         fig.savefig('static/attempts.png', dpi=108)
         plt.show()
         return redirect('static/attempts.png')
-
-        # ax.bar(song_attempts['statsUniqueString'], song_attempts['timesAttemptedDifficulty'][4], width, label='Times Attempted')
-        # ax.bar(song_attempts['statsUniqueString'], song_attempts['timesCompletedDifficulty'][4], width, bottom=song_attempts['timesAttemptedDifficulty'][4], label='Times Completed')
-        # plt.show()
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=80, debug=True)
