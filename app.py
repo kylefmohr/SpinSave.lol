603ff87511e1da487ecdac9008a8904c84089ad3
Kyle Mohr <6644803+kylefmohr@users.noreply.github.com>
2022-05-18 10:42:33 -0500

    preliminary support for custom songs from spinsha.re

--- app.py
+++ app.py
@@ -1,7 +1,7 @@
 import json
 from collections import OrderedDict
 
-
+import requests
 import matplotlib
 matplotlib.use('Agg')
 import matplotlib.pyplot as plt
@@ -10,6 +10,24 @@ from flask import Flask, render_template, request, redirect
 from werkzeug.utils import secure_filename
 
 app = Flask(__name__)
+
+def custom_string_to_title(title):
+    title1 = title.split('_')[1]
+    title2 = title.split('_')[2]
+    fileReference = title1 + "_" + title2
+    if "spinshare_" not in fileReference:
+        return ''
+    else:
+        url = "https://spinsha.re/api/song/" + fileReference
+        r = requests.get(url)
+        data = r.json()
+        data = data['data']
+        try:
+            title = data['title']
+        except:
+            title = ''
+        print(fileReference + ": " + title)
+        return title
 
 
 @app.route('/')
@@ -29,25 +47,30 @@ def work_with_file():
 
         json_data = json.loads(unparsed_data)
         song_attempts_data = json_data['largeStringValuesContainer']['values']
-
+        
+        custom_or_original = request.values.get("custom")
+        
         difficulty_index = {'easy': 0, 'medium': 1, 'hard': 2, 'expert': 3, 'XD': 4}  # save file records song attempts/completions as a 5 item array
         difficulty = request.values.get('difficulty')
         difficulty_index = difficulty_index[difficulty]
 
         sort_by = request.values.get('sort')
 
-        custom_song_attempts = {}
-        regular_song_attempts = {}
-        for current_song in song_attempts_data:
+
+        song_attempts = {}
+        for current_song in song_attempts_data:  # separate songs into custom and regular, and remove extraneous entries
             if current_song['key'] == 'MouseUserInputMapping' or current_song['key'] == 'KeyboardUserInputMapping':
                 pass
             else:
                 if current_song['key'].startswith('CUSTOM'):
-                    custom_song_attempts[current_song['key']] = current_song['val']
+                    if custom_or_original == 'custom':
+                        song_attempts[current_song['key']] = current_song['val']
+
                 elif 'Random' in current_song['key'] or 'Calibration' in current_song['key'] or 'Create Custom' in current_song['key'] or 'CreateCustomTrack' in current_song['key']:   # doesn't accurately show how many times you've attempted a random song, calibration tool value seems incorrect as well
                     pass
                 else:
-                    regular_song_attempts[current_song['key']] = current_song['val']
+                    if custom_or_original == 'original':
+                        song_attempts[current_song['key']] = current_song['val']
 
         plt.ioff()
         width = 1
@@ -55,24 +78,42 @@ def work_with_file():
 
         fig = plt.figure(figsize=(18, 14), dpi=my_dpi)
 
-        song_attempts = json.loads(json.dumps(regular_song_attempts))
+        song_attempts = json.loads(json.dumps(song_attempts))
         song_attempts = dict(song_attempts.items())
         song_attempts = {k: v for k, v in song_attempts.items() if v is not None and v != 0 and v != ''}
         song_attempts = dict(song_attempts.items())
 
         stats = list(song_attempts.values())
-        #stats = sorted(stats[difficulty_index])
         stats = json.loads(json.dumps(stats))
         new_stats = {}
-        final_stats = {'song_title': [], 'song_attempts': [], 'song_completions': []}
+        final_stats = {'song_title': [], 'song_attempts': [], 'song_completions': [], 'high_score': [], 'best_accuracy': [], 'best_streak': []}
+        custom_lookups = 0
         for i in range(len(stats)):
             new_stats[i] = json.loads(stats[i])
             new_stats[i] = new_stats[i].values()
             new_stats[i] = list(new_stats[i])
-            final_stats['song_title'].append(new_stats[i][6][:-6])
-            final_stats['song_attempts'].append(new_stats[i][3][difficulty_index])
-            final_stats['song_completions'].append(new_stats[i][4][difficulty_index])
+            try:
+                if custom_or_original == 'custom':
+                    if custom_lookups <= 50:  # limit to 50 custom songs, otherwise we risk overloading the spinshare api, and also it doesn't look good on a bar graph
+                        title = custom_string_to_title(new_stats[i][6][:-6])
+                        if title == '':
+                            # go to next index in for loop
+                            continue
+                        else:
+                            custom_lookups += 1
+                            final_stats['song_title'].append(title)
+                else:
+                    final_stats['song_title'].append(new_stats[i][6][:-6])
+                final_stats['song_attempts'].append(new_stats[i][3][difficulty_index])
+                final_stats['song_completions'].append(new_stats[i][4][difficulty_index])
+                final_stats['high_score'].append(new_stats[i][0][difficulty_index]['valueForDifficulty'])
+                final_stats['best_accuracy'].append(new_stats[i][1][difficulty_index]['valueForDifficulty'])
+                final_stats['best_streak'].append(new_stats[i][2][difficulty_index]['valueForDifficulty'])
+            except IndexError:
+                pass
 
+        # print(final_stats['best_accuracy'])
+        # print(final_stats['best_streak'])
         if sort_by == 'Attempts':
             zipped_lists = zip(final_stats['song_attempts'], final_stats['song_completions'], final_stats['song_title'])  # whichever dict is listed first will be the one that gets sorted
             sorted_stats = sorted(zipped_lists, reverse=True)
